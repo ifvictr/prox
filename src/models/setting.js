dcddbf1f3179695c716eb31d84b8746c7e5975f4
Victor Truong <me@ifvictr.com>
2019-10-04 02:01:09 -0700

    Remove non-working autoIndex option

--- src/models/setting.js
+++ src/models/setting.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose'
 const SettingSchema = new mongoose.Schema({
     name: String,
     value: String
-}, { autoIndex: false })
+})
 
 export default mongoose.model('Setting', SettingSchema)
