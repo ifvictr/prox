c219921cf7bc35630d15fd75fa3dd81b7a2c439c
Victor Truong <victor@victortruong.com>
2020-04-06 02:45:01 -0700

    Replace message instead of actually deleting

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -17,18 +17,23 @@ export default async (bot, message, args) => {
 
     // Check if target is post or thread reply
     let messageId
+    let post
     if (isUrl(args[1])) { // Is URL?
         messageId = getIdFromUrl(args[1])
         if (!messageId) {
             await bot.replyEphemeral(message, 'Couldn’t extract a message ID from the given URL.')
             return
         }
+
+        // Attempt to find a post the link was pointing to
+        post = await Post.findOne({ postMessageId: messageId })
     } else if (!isNaN(args[1])) { // Is post number?
-        const post = await Post.findOne({ postNumber: args[1] })
+        post = await Post.findOne({ postNumber: args[1] })
         if (!post) {
             await bot.replyEphemeral(message, 'The specified post couldn’t be found.')
             return
         }
+
         messageId = post.postMessageId
     } else { // Is invalid input
         await bot.replyEphemeral(message, 'Input must be a post number or message URL.')
@@ -37,10 +42,12 @@ export default async (bot, message, args) => {
 
     // Delete the message using retrieved ID
     try {
-        await bot.deleteMessage({
+        const updatedMessage = {
             id: messageId,
             conversation: { id: process.env.SLACK_POST_CHANNEL_ID },
-        })
+            text: `:skull: ${post ? `*#${post.postNumber}:* _This post` : '_This message'} has been deleted._`
+        }
+        await bot.updateMessage(updatedMessage)
         await bot.replyEphemeral(message, 'Message deleted.')
     } catch (e) {
         await bot.replyEphemeral(message, `Failed to delete. Reason: \`${e.data.error}\``)
