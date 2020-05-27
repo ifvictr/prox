10a8bd663dff26ac93597da5e46798964e2465e2
Victor Truong <victor@victortruong.com>
2020-05-27 16:09:55 -0700

    Exclude bot messages from post submission

--- src/features/post_submit.js
+++ src/features/post_submit.js
@@ -1,11 +1,11 @@
 import config from '../config'
-import { channelType, threaded } from '../middlewares'
+import { channelType, noBotMessages, threaded } from '../middlewares'
 import { createSubmission } from '../utils/slack'
 
 export default app => {
     // Match non-command and non-reply DMs
     const messagePattern = /^(?!\/)(?!(#*)\d+:(\s|$))/
-    app.message(channelType('im'), threaded(false), messagePattern, async ({ client, event, say }) => {
+    app.message(channelType('im'), threaded(false), noBotMessages, messagePattern, async ({ client, event, say }) => {
         await say(':mag: Your submission is now under review. If you were trying to send an anonymous reply, resend it in the following format: `<post number>: <your message here>` (i.e., sending `1337: hello` would send “hello” to post *#1337*).')
         await createSubmission(client, config.reviewChannelId, event)
     })
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -10,4 +10,6 @@ export const channel = id => filterEvents(event => event.channel === id)
 
 export const channelType = type => filterEvents(event => event.channel_type === type)
 
+export const noBotMessages = filterEvents(event => !('subtype' in event) || event.subtype !== 'bot_message')
+
 export const threaded = (shouldBeThreaded = true) => filterEvents(event => 'thread_ts' in event === shouldBeThreaded)
