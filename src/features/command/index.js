16a64aa547cfa20c0097cb28b333c83176237a23
Victor Truong <victor@victortruong.com>
2020-05-27 02:09:28 -0700

    Tolerate accidental double spaces in command

--- src/features/command/index.js
+++ src/features/command/index.js
@@ -10,7 +10,7 @@ export default app => {
     app.command('/prox', async ({ ack, client, command }) => {
         await ack()
 
-        const args = command.text.split(' ')
+        const args = command.text.split(/\s+/)
         const subcommand = args[0].toLowerCase()
         if (!subcommands.has(subcommand)) {
             await sendEphemeralMessage(client, command.channel_id, command.user_id, 'Subcommand not found.')
