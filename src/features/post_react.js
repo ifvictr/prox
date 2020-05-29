28d0723c093444b696215818a916ce734021ffa2
Victor Truong <victor@victortruong.com>
2020-05-28 20:33:13 -0700

    Make comments less verbose

--- src/features/post_react.js
+++ src/features/post_react.js
@@ -5,21 +5,18 @@ import { getMessage, sendEphemeralMessage } from '../utils/slack'
 
 export default app => {
     app.event('reaction_added', async ({ client, context, event }) => {
-        // Reaction needs to be sent in post channel
+        // Needs to be in the post channel
         if (event.item.channel !== config.postChannelId) {
             return
         }
 
-        // Reaction needs to be sent to anon reply
+        // Needs to be sent to an anon reply
         const message = await getMessage(client, event.item.channel, event.item.ts)
-        if (!message.thread_ts) {
-            return
-        }
-        if (message.bot_id === context.botId && !message.username) {
+        if (!message.thread_ts || (message.bot_id === context.botId && !message.username)) {
             return
         }
 
-        // Reaction needs to be from OP, sent to a message threaded under their Prox post
+        // Needs to be from the post author to a reply under their post
         const post = await Post.findOne({ postMessageId: message.thread_ts })
         const senderIdHash = hash(event.user, post.salt)
         if (!post || post.authorIdHash !== senderIdHash) {
