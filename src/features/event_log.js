af2deb7e81b83b369d395204587de944a7151cac
Victor Truong <victor@victortruong.com>
2020-05-29 19:59:53 -0700

    Log message deletions from external sources

--- src/features/event_log.js
+++ src/features/event_log.js
@@ -1,5 +1,7 @@
+import { subtype } from '@slack/bolt'
 import config from '../config'
 import { channel } from '../middlewares'
+import { removeSpecialTags } from '../utils'
 import { sendMessage } from '../utils/slack'
 
 export default app => {
@@ -9,5 +11,16 @@ export default app => {
 
     app.event('member_left_channel', channel(config.postChannelId), async ({ client, event }) => {
         await sendMessage(client, config.streamChannelId, `_Someone left <#${event.channel}>._`)
+    })
+
+    app.message(subtype('message_deleted'), channel(config.postChannelId), async ({ client, context, message }) => {
+        // Only log deletions of Prox's messages
+        const deletedMessage = message.previous_message
+        if (!(deletedMessage.user === context.botUserId || deletedMessage.bot_id === context.botId)) {
+            return
+        }
+
+        const displayName = deletedMessage.user ? `<@${deletedMessage.user}>` : deletedMessage.username
+        await sendMessage(client, config.streamChannelId, `_A ${!deletedMessage.thread_ts ? 'post' : 'message'} from ${displayName} was deleted in <#${config.postChannelId}>:_\n>>> ${removeSpecialTags(deletedMessage.text)}`)
     })
 }
