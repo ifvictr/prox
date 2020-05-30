1f93b9be825b4f3c3666d6d62472db3dbdcae2f1
Victor Truong <victor@victortruong.com>
2020-05-30 00:38:44 -0700

    Add anon reactions to submission success message

--- src/features/post_submit.js
+++ src/features/post_submit.js
@@ -6,7 +6,9 @@ export default app => {
     // Match non-command and non-reply DMs
     const messagePattern = /^(?!\/)(?!(#*)\d+:(\s|$))/
     app.message(channelType('im'), threaded(false), noBotMessages, messagePattern, async ({ client, event, say }) => {
-        await say(':mag: Your submission is now under review. If you were trying to send an anonymous reply, resend it in the following format: `<post number>: <your message here>` (i.e., sending `1337: hello` would send “hello” to post *#1337*).')
+        await say(`:mag: Your submission is now under review. Two quick things:
+- If you were trying to send an anonymous reply, resend the message in the following format: \`<post number>: <your message here>\` (i.e., sending \`1337: hello\` would send “hello” to post *#1337*).
+- If this is approved, you (and only you, the author) will be able to send anonymous reactions to the replies under your post by using the *Add anonymous reactions* shortcut, found under *More actions* → *More message shortcuts*.`)
         await createSubmission(client, config.reviewChannelId, event)
     })
 }
