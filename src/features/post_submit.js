9df427becd310ec34aa8dd3b5993e9c9c0669d8c
Victor Truong <victor@victortruong.com>
2020-06-20 18:53:09 -0700

    Change submission confirm message

--- src/features/post_submit.js
+++ src/features/post_submit.js
@@ -7,7 +7,7 @@ export default app => {
     const messagePattern = /^(?!\/)(?!(#*)\d+:(\s|$))/
     app.message(channelType('im'), threaded(false), noBotMessages, messagePattern, async ({ client, event, say }) => {
         await say(`:mag: Your submission is now under review. Two quick things:
-- If you were trying to send an anonymous reply, resend the message in the following format: \`<post number>: <your message here>\` (i.e., sending \`1337: hello\` would send “hello” to post *#1337*).
+- If you were trying to send an anonymous reply, resend the message in the following format: \`<post number>: <your message here>\` (i.e., sending \`16: hello\` would send “hello” to post *#16*).
 - If this is approved, you (and only you, the author) will be able to send anonymous reactions to the replies under your post by using the *Add anonymous reactions* shortcut, found under *More actions* → *More message shortcuts*.`)
         await createSubmission(client, config.reviewChannelId, event)
     })
