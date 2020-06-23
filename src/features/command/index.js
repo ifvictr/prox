83d87be9046412c6c684fbbd0e50627be8f86450
Victor Truong <victor@victortruong.com>
2020-06-23 04:23:23 -0700

    Use shorter command for help

--- src/features/command/index.js
+++ src/features/command/index.js
@@ -22,7 +22,7 @@ export default app => {
         const args = command.text.split(/\s+/)
         const subcommand = args[0].toLowerCase()
         if (!subcommands.has(subcommand)) {
-            await sendEphemeralMessage(client, command.channel_id, command.user_id, `Subcommand not found. To see all the available commands, run \`${command.command} help\`.`)
+            await sendEphemeralMessage(client, command.channel_id, command.user_id, `Subcommand not found. To see all the available commands, run \`${command.command}\`.`)
             return
         }
 
