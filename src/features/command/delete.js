8cc1343a01bb73b5bad89299a5bd41088f6d6857
Saharsh Yeruva <github@saharsh.tech>
2020-10-05 18:34:55 -0400

    Update delete.js

--- src/features/command/delete.js
+++ src/features/command/delete.js
@@ -69,7 +69,7 @@ export default async ({ client, command, respond }, args) => {
       await sendMessage(
         client,
         config.streamChannelId,
-        `_<@${command.user_id}> deleted *#${post.postNumber}*._`
+        `_<@${command.user_name}> deleted *#${post.postNumber}*._`
       )
     }
 
